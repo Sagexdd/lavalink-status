9abb443d00b743c8c8102ce7bbe541e27edaf293
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-08-03 13:12:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
+// Configuration settings for Lavalink REST API
 module.exports = {
-    LAVALINK: {
-        HOST: "pnode1.danbot.host", // or IP, e.g., "http://127.0.0.1"
-        PORT: 1186, // Replace with your actual Lavalink port
-        PASSWORD: "Yuvraj.apk#001",
-        SECURE: false // set to true if you're using HTTPS (SSL)
+    LAVALINK_V3: {
+      HOST: "https://v3.lavalink.rocks/v3",
+      PASSWORD: "horizxon.tech",
     },
-    API_TIMEOUT: 50000
-};
+    LAVALINK_V4: {
+      HOST: "pnode1.danbot.host:1186",
+      PASSWORD: "Yuvraj.apk#001",
+    },
+    PORT: 3000,
+    API_TIMEOUT: 50000,
+  };
+  
