104aeed5352efbf0ee85d53c2c130d2a8dcdebe5
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-08-03 13:08:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     LAVALINK: {
         HOST: "pnode1.danbot.host", // or IP, e.g., "http://127.0.0.1"
         PORT: 1186, // Replace with your actual Lavalink port
-        PASSWORD: "Harmonix",
+        PASSWORD: "Yuvraj.apk#001",
         SECURE: false // set to true if you're using HTTPS (SSL)
     },
     API_TIMEOUT: 50000
